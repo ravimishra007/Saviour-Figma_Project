82b012b7aec0075ee19d3d7a96ccffeae56ecb0a
ravi mishra <ravimishrafea.560722@gmail.com>
2024-06-02 13:32:18 +0530

    figma project done

---
+++ saviour_app/src/App.jsx
@@ -0,0 +1,18 @@
+
+import { Details } from './components/Details'
+import { Events } from './components/Events'
+import { Header } from './components/Header'
+
+function App() {
+
+  return (
+    <>
+        <Header/>
+        <Details/>
+        <Events/>
+
+    </>
+  )
+}
+
+export default App
